f130d3c87d2a122462d3315152e9370f701b82a8
sekercih <hkkskrc@gmail.com>
2021-05-15 18:06:05 +0300

    basic

---
+++ src/Ogrenci.java
@@ -0,0 +1,27 @@
+public class Ogrenci extends Kisi {
+
+    private String numara;
+    private String sinif;
+
+    public String getNumara() {
+        return numara;
+    }
+
+    public void setNumara(String numara) {
+        this.numara = numara;
+    }
+
+    public Ogrenci(String adSoyad, String kimlikNo, int yas, String numara, String sinif) {
+        super(adSoyad, kimlikNo, yas);
+        this.numara = numara;
+        this.sinif = sinif;
+    }
+
+    public String getSinif() {
+        return sinif;
+    }
+
+    public void setSinif(String sinif) {
+        this.sinif = sinif;
+    }
+}
---
+++ src/Running.java
@@ -0,0 +1,8 @@
+public class Running {
+    public static void main(String[] args) {
+        Islemler islem = new Islemler();
+        islem.testKisiOlustur();
+        islem.anaMenu();
+    }
+}
+
